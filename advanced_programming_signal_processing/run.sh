964d8b53e00fc145033aa905e98ec1f6f13adcea
sozaki5000 <gjmptw50716@gmail.com>
2025-06-24 16:10:22 +0900

    modify threshold, enable convert -equalize

--- advanced_programming_signal_processing/run.sh
+++ advanced_programming_signal_processing/run.sh
@@ -6,21 +6,21 @@ for image in $1/test/*.ppm; do
     name="imgproc/"$bname
     x=0    	#
     echo $name
-    convert "${image}" "${name}"  # 何もしない画像処理
+    # convert "${image}" "${name}"  # 何もしない画像処理
 #   convert -blur 2x6 "${image}" "${name}"
-#   convert -median 3 "${image}" "${name}"
+    # convert -median 1 "${image}" "${name}"
 #   convert -auto-level "${image}" "${name}"
-#   convert -equalize "${image}" "${name}"
+    convert -equalize "${image}" "${name}"
     rotation=0
     echo $bname:
     for template in $1/*.ppm; do
 	echo `basename ${template}`
 	if [ $x = 0 ]
 	then
-	    ./matching $name "${template}" $rotation 0.5 cp 
+	    ./matching $name "${template}" $rotation 1.5 cwp 
 	    x=1
 	else
-	    ./matching $name "${template}" $rotation 0.5 p 
+	    ./matching $name "${template}" $rotation 1.5 wp 
 	fi
     done
     echo ""
