0577b2a7ceb07145b182fc3188bcaac0120adef4
atsushi2965 <atsushi0523.kasuya@gmail.com>
2025-07-01 14:27:03 +0900

    level6

--- advanced_programming_signal_processing/run.sh
+++ advanced_programming_signal_processing/run.sh
@@ -1,28 +1,43 @@
 #!/bin/sh
 # imagemagickで何か画像処理をして，/imgprocにかきこみ，テンプレートマッチング
 # 最終テストは，直下のforループを次に変更 for image in $1/final/*.ppm; do
+r="0 90 180 270"
+for template in $1/*.ppm; do
+	btpl=`basename ${template}`
+	for rotation in $r; do
+	        tpl="rotated/"$rotation"/"$btpl
+		convert -rotate $rotation "${template}" "${tpl}"
+	done
+done
+
 for image in $1/test/*.ppm; do
-    bname=`basename ${image}`
-    name="imgproc/"$bname
-    x=0    	#
-    echo $name
-    # convert "${image}" "${name}"  # 何もしない画像処理
-#   convert -blur 2x6 "${image}" "${name}"
-    # convert -median 1 "${image}" "${name}"
-#   convert -auto-level "${image}" "${name}"
-    convert -equalize "${image}" "${name}"
-    rotation=0
-    echo $bname:
-    for template in $1/*.ppm; do
-	echo `basename ${template}`
-	if [ $x = 0 ]
-	then
-	    ./matching $name "${template}" $rotation 1.5 cwp 
-	    x=1
-	else
-	    ./matching $name "${template}" $rotation 1.5 wp 
-	fi
-    done
-    echo ""
+	bname=`basename ${image}`
+	name="imgproc/"$bname
+	x=0    	#
+	echo $name
+	# convert "${image}" "${name}"  # 何もしない画像処理
+	#   convert -blur 2x6 "${image}" "${name}"
+	# convert -median 1 "${image}" "${name}"
+	#   convert -auto-level "${image}" "${name}"
+	convert -equalize "${image}" "${name}"
+	echo $bname:
+	for template in $1/*.ppm; do
+		echo `basename ${template}`
+		btpl=`basename ${template}`
+		if [ $x = 0 ]
+		then
+			printf '%s ' $r | xargs -d' ' -I{} -P4 ./matching $name "rotated/{}/${btpl}" {} 1.5 cwp 
+			# for rotation in $r; do
+			# 	./matching $name "rotated/${rotation}/${btpl}" $rotation 1.5 cwp 
+			# done
+			x=1
+		else
+			printf '%s ' $r | xargs -d' ' -I{} -P4 ./matching $name "rotated/{}/${btpl}" {} 1.5 wp 
+			# for rotation in $r; do
+			# 	./matching $name "rotated/${rotation}/${btpl}" $rotation 1.5 wp 
+			# done
+		fi
+	done
+	echo ""
 done
 wait
